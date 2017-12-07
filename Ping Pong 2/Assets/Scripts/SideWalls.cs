67eb9bd0963de4713ccada52c12eca06e9840537
Darren Buttigieg <b42085@mcast.org.mt>
2017-12-07 11:12:38 +0100

    Score Working yet i need to reset the ball after a goal

---
+++ Ping Pong 2/Assets/Scripts/SideWalls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour {
+    private LevelManager myLevelManager;
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+         Debug.Log(hitInfo.name);
+        if (hitInfo.name == "ball")
+        {
+            string wallName = transform.name;
+            Console.Write(wallName); Score.Scores(wallName);
+           
+
+            myLevelManager.LoadLevel("Level1");
+            
+        
+        }
+    }
+}
+
---
+++ Ping Pong/Assets/Scripts/PlayerControl2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl2 : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseUnits = (Input.mousePosition.y / Screen.height * 13f) - 6.5f;
+        float mousePos = Mathf.Clamp(mouseUnits, -4.2f, 2.3f);
+        Vector3 paddlePosition = new Vector3(2.72f, mousePos, this.transform.position.x);
+        this.transform.position = paddlePosition;
+		
+	}
+}

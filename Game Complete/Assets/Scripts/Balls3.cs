d542f51a3f33c06d521d8890db04ebe39efe8542
Darrenb245 <32862542+Darrenb245@users.noreply.github.com>
2017-12-17 14:30:35 +0100

    Game ready
    Game is ready with Level 1 to 3
    Score continues with winning scene at the end.

---
+++ Game Complete/Assets/Scripts/Balls3.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls3 : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+    }
+
+    float time = -1;
+    void Update()
+    {
+        if (this.transform.position.x <= -6.54 || this.transform.position.x >= 6.54)
+        {
+            this.transform.position = new Vector3(-0.05f, 0.23f, 1.23f);
+            time = 1;
+
+        }
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            this.transform.position = new Vector3(-0.05f, 0.23f, 1.23f);
+        }
+    
+
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Player"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+
+}
---
+++ Game Complete/Assets/Scripts/Score.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    public int num;
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+    public static int levelscore1 = 0;
+    public static int levelscore2 = 0;
+    public GUISkin layout;
+
+    GameObject theBall;
+
+    // Use this for initialization
+    void Start()
+    {
+        theBall = GameObject.FindGameObjectWithTag("Ball");
+    }
+
+    public static void Scores(string wallID)
+    {
+        
+        if (wallID == "RightWall")
+        {
+         
+            PlayerScore1++;
+            levelscore1++;
+            
+        }
+        else
+        {
+
+            
+            PlayerScore2++;
+            levelscore2++;
+        }
+         
+        if (levelscore1 == 3|| levelscore2 == 3)
+        {
+            
+            levelscore1 = 0;
+            levelscore2 = 0;
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            if (currentScene == 3)
+            {
+                if (PlayerScore1 > PlayerScore2)
+                {
+                    SceneManager.LoadScene(currentScene + 1);
+                }
+                else
+                {
+                    SceneManager.LoadScene(currentScene + 2);
+               }
+            }
+            else
+            {
+                SceneManager.LoadScene(currentScene + 1);
+           }
+     
+        }
+        
+
+
+    }
+
+
+
+    void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+
+       
+
+    }
+}
+    

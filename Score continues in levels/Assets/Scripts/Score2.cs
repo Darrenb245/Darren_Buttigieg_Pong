81ebd91d428dce54a79b0531afaf74a217113ff8
Darrenb245 <32862542+Darrenb245@users.noreply.github.com>
2017-12-17 14:28:44 +0100

    Score continues
    Score is contuining from level 1 to level 3 to see who wins.

---
+++ Score continues in levels/Assets/Scripts/Score2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Score2 : MonoBehaviour
+{
+    public int num;
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+
+    public GUISkin layout;
+
+    GameObject theBall;
+
+    // Use this for initialization
+    void Start()
+    {
+        theBall = GameObject.FindGameObjectWithTag("Ball");
+    }
+
+    public static void Scores(string wallID)
+    {
+        Debug.Log(PlayerScore2);
+        if (wallID == "RightWall")
+        {
+            PlayerScore1++;
+
+        }
+        else
+        {
+
+
+            PlayerScore2++;
+        }
+        Debug.Log(PlayerScore1);
+
+        if (PlayerScore1 == 7 || PlayerScore2 == 7)
+        {
+        
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //SceneManager.LoadScene("Level2");
+        }
+
+
+
+    }
+
+
+
+
+    void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+
+
+
+    }
+}
+
+
---
+++ Score continues in levels/Assets/Scripts/SideWalls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour {
+    private LevelManager myLevelManager;
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+         Debug.Log(hitInfo.name);
+        if (hitInfo.name == "ball")
+        {
+            string wallName = transform.name;
+           Score.Scores(wallName);
+           
+
+            //myLevelManager.LoadLevel("Level1");
+            
+        
+        }
+
+   
+    }
+}
+

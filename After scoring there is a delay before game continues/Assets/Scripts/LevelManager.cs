35ac3b68bf6a40f368b27c49befbfd5b3402ab56
Darrenb245 <32862542+Darrenb245@users.noreply.github.com>
2017-12-17 14:24:29 +0100

    After scoring there will be a delay
    This will happen so they give a chance so the players to re-adjust

---
+++ After scoring there is a delay before game continues/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string levelname)
+    {
+        Debug.Log("Level1: " + levelname);
+        //loads the scene named levelName
+        SceneManager.LoadScene(levelname);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void loadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
+   

5383bd687407f9cfb6e1c9732a304c19e82ce7a0
Darrenb245 <32862542+Darrenb245@users.noreply.github.com>
2017-12-17 14:26:45 +0100

    all levels have the ball resetting after scoring
    Every level has now complete scoring and resetting of the ball.

---
+++ All levels reset after scoring/Assets/Scripts/Balls.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+    }
+
+   
+  void Update()
+    {
+        if(this.transform.position.x<=-4.36 || this.transform.position.x >= 4.36)
+        {
+
+            this.transform.position = new Vector3(0f, 0.15f, 1.23f);
+           
+           
+        }
+        
+        
+
+
+    }
+
+   void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Player"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+
+}
---
+++ All levels reset after scoring/Assets/Scripts/Balls2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls2 : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+    }
+
+    float time = -1;
+    void Update()
+    {
+        if (this.transform.position.x <= -6.55 || this.transform.position.x >= 6.55)
+        {
+        }
+    
+
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Player"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+
+}
+
+
